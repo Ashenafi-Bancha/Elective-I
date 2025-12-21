663d7660728cf2392c07b7e7b9cedc50ac5612d7
Ashenafi Bancha <156710615+Ashenafi-Bancha@users.noreply.github.com>
2025-12-21 21:59:55 +0300

    Define Pokemon class with properties and method

--- WebAPIs/ControllerApi/PokemonControllerApi/Pokemon.cs
+++ WebAPIs/ControllerApi/PokemonControllerApi/Pokemon.cs
@@ -1,1 +1,15 @@
+//1. Define the Pokemon class
+public class Pokemon
+{
+    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Level { get; set; }
+    
+    public void GainExperience(int amount)
+    {
+        Level += amount;
+        
 
+    }
+}
