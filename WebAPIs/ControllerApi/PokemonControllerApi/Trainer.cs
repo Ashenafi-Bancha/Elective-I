437f99cc91b40da80ed65b6b3eb8b75ec9002131
Ashenafi Bancha <156710615+Ashenafi-Bancha@users.noreply.github.com>
2025-12-21 22:03:56 +0300

    Add Trainer class with properties for trainer details

--- WebAPIs/ControllerApi/PokemonControllerApi/Trainer.cs
+++ WebAPIs/ControllerApi/PokemonControllerApi/Trainer.cs
@@ -1,1 +1,8 @@
-
+public class Trainer
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; }
+    public string City { get; set; } = string.Empty;
+    public List<string> PokemonNames { get; set; } = new List<string>();
+}
