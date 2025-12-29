891a5ee0b2b7e8f519639a8d2b6432874504a064
Ashenafi Bancha <156710615+Ashenafi-Bancha@users.noreply.github.com>
2025-12-29 23:21:11 +0300

    Set up basic ASP.NET Core application with Swagger

--- WebAPIs/ControllerApi/PokemonControllerApi/Program.cs
+++ WebAPIs/ControllerApi/PokemonControllerApi/Program.cs
@@ -1,1 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+builder.Services.AddEndPointsExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.MapGet("/", ()=> "Welcome to the Controller Based PokemonApi");
+
+app.UseHttpsRedirection();
+
+app.Run();
